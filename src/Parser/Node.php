bd1414640c95bbfdc07256b5c8ae6e89028e1875
Max Zheltikov <maxcruer@gmail.com>
2021-08-11 22:39:46 +0300

    working on Node

--- src/Parser/Node.php
+++ src/Parser/Node.php
@@ -4,4 +4,13 @@ namespace Zheltikov\TypeAssert\Parser;
 
 class Node
 {
+    /**
+     * @var Type
+     */
+    private $type;
+
+    public function __construct(Type $type)
+    {
+        $this->type = $type;
+    }
 }
