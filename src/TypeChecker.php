358f3bb2aa7428a2cf2c1f05a0e924df9419da84
Max Zheltikov <maxcruer@gmail.com>
2021-08-17 19:42:39 +0300

    added some more type handling

--- src/TypeChecker.php
+++ src/TypeChecker.php
@@ -129,18 +129,233 @@ final class TypeChecker
                 invariant($ast->countChildren() === 1, 'Nullable Node must have exactly 1 child.');
 
                 $new_ast = (new Node(Type::UNION()))
-                    ->appendChild(new Node(Type::NULL()))
-                    ->appendChild($ast->getChildAt(0));
+                    ->appendChildren(
+                        [
+                            new Node(Type::NULL()),
+                            clone $ast->getChildAt(0),
+                        ]
+                    );
 
                 return self::astToCheckerFn($new_ast);
+
+            case Type::TUPLE()->getKey():
+            case Type::SHAPE()->getKey():
+            case Type::LIST()->getKey():
+                break;
+
+            case Type::BOOL()->getKey():
+                return function ($value): bool {
+                    return is_bool($value);
+                };
+
+            case Type::INT()->getKey():
+                return function ($value): bool {
+                    return is_int($value);
+                };
+
+            case Type::FLOAT()->getKey():
+                return function ($value): bool {
+                    return is_float($value);
+                };
+
+            case Type::STRING()->getKey():
+                return function ($value): bool {
+                    return is_string($value);
+                };
+
+            case Type::ARRAY()->getKey():
+                return function ($value): bool {
+                    return is_array($value);
+                };
+
+            case Type::OBJECT()->getKey():
+                return function ($value): bool {
+                    return is_object($value);
+                };
+
+            case Type::CALLABLE()->getKey():
+                return function ($value): bool {
+                    return is_callable($value);
+                };
+
+            case Type::ITERABLE()->getKey():
+                return function ($value): bool {
+                    return is_iterable($value);
+                };
+
+            case Type::RESOURCE()->getKey():
+                return function ($value): bool {
+                    return is_resource($value);
+                };
+
+            case Type::NULL()->getKey():
+                return function ($value): bool {
+                    return $value === null;
+                };
+
+            case Type::NEGATED()->getKey():
+                invariant($ast->countChildren() === 1, 'Negated Node must have exactly 1 child.');
+
+                $fn = self::astToCheckerFn($ast->getChildAt(0));
+
+                return function ($value) use ($fn): bool {
+                    return !$fn($value);
+                };
+
+            case Type::USER_DEFINED()->getKey():
+                $type = $ast->getValue();
+
+                invariant(
+                    class_exists($type) || interface_exists($type),
+                    'User-defined type must exist.'
+                );
+
+                return function ($value) use ($type): bool {
+                    // TODO: instanceof doesn't work with traits :(
+                    return $value instanceof $type;
+                };
+
+            case Type::ARRAYKEY()->getKey():
+                $new_ast = (new Node(Type::UNION()))
+                    ->appendChildren(
+                        [
+                            new Node(Type::STRING()),
+                            new Node(Type::INT()),
+                        ]
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::NOT_NULL()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::NULL()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::SCALAR()->getKey():
+                return function ($value): bool {
+                    return is_scalar($value);
+                };
+
+            case Type::NUMBER()->getKey():
+                $new_ast = (new Node(Type::UNION()))
+                    ->appendChildren(
+                        [
+                            new Node(Type::INT()),
+                            new Node(Type::FLOAT()),
+                        ]
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::MIXED()->getKey():
+                return function (): bool {
+                    return true;
+                };
+
+            case Type::VOID()->getKey():
+                return function (): bool {
+                    return false;
+                };
+
+            // TODO: stuff from php-arrays...
+
+            case Type::NOT_EMPTY()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::EMPTY()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::EMPTY()->getKey():
+                return function ($value): bool {
+                    return empty($value);
+                };
+
+            case Type::TRUE()->getKey():
+                return function ($value): bool {
+                    return $value === true;
+                };
+
+            case Type::FALSE()->getKey():
+                return function ($value): bool {
+                    return $value === false;
+                };
+
+            case Type::NOT_TRUE()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::TRUE()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::NOT_FALSE()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::FALSE()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::POSITIVE()->getKey():
+                return function ($value): bool {
+                    return $value > 0;
+                };
+
+            case Type::NEGATIVE()->getKey():
+                return function ($value): bool {
+                    return $value < 0;
+                };
+
+            case Type::NOT_POSITIVE()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::POSITIVE()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::NOT_NEGATIVE()->getKey():
+                $new_ast = (new Node(Type::NEGATED()))
+                    ->appendChild(
+                        new Node(Type::NEGATIVE()),
+                    );
+
+                return self::astToCheckerFn($new_ast);
+
+            case Type::RAW_STRING()->getKey():
+                $raw_string = $ast->getValue();
+
+                invariant($raw_string !== null, 'Raw string Node must have a value.');
+
+                return function ($value) use ($raw_string): bool {
+                    return $value === $raw_string;
+                };
+
+            case Type::KEY_VALUE_PAIR()->getKey():
+                break;
 
             default:
                 throw new InvalidArgumentException(
                     sprintf(
-                        'Unknown Node type: %s',
-                        $ast->getType()->getKey()
+                        'Unknown Node type %s%s',
+                        $ast->getType()->getKey(),
+                        $ast->getValue() !== null
+                            ? ' with value ' . ((string) $ast->getValue())
+                            : ''
                     )
                 );
         }
+
+        throw new RuntimeException(
+            sprintf(
+                'Handling of type %s not yet implemented!',
+                $ast->getType()->getKey()
+            )
+        );
     }
 }
