79b09dce3a517afa53ab28eeaa7625e72de81b93
Max Zheltikov <maxcruer@gmail.com>
2021-08-26 21:59:42 +0300

    added a flag to toggle reporting

--- src/TypeCheckerState.php
+++ src/TypeCheckerState.php
@@ -10,11 +10,17 @@ class TypeCheckerState
     protected array $report_stack;
 
     /**
+     * @var bool
+     */
+    protected bool $reporting_enabled;
+
+    /**
      *
      */
     private function __construct()
     {
         $this->report_stack = [];
+        $this->setReportingEnabled();
     }
 
     // -------------------------------------------------------------------------
@@ -35,7 +41,9 @@ class TypeCheckerState
      */
     public function appendReportStack(string $message): self
     {
-        $this->report_stack[] = $message;
+        if ($this->isReportingEnabled()) {
+            $this->report_stack[] = $message;
+        }
         return $this;
     }
 
@@ -45,5 +53,23 @@ class TypeCheckerState
     public function getReportStack(): array
     {
         return $this->report_stack;
+    }
+
+    /**
+     * @param bool $reporting_enabled
+     * @return $this
+     */
+    public function setReportingEnabled(bool $reporting_enabled = true): self
+    {
+        $this->reporting_enabled = $reporting_enabled;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReportingEnabled(): bool
+    {
+        return $this->reporting_enabled;
     }
 }
